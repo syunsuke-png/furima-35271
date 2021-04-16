1eee23b9cc23158a143339cfb8a8e513fbc1313f
syunsuke-png <flittydfil@icloud.com>
2021-04-16 18:20:08 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :barth_day, presence: true
+
+  has_many :items
+  has_many :purchases
 end
